4702ea61a61690ca1b71171f4c50ef07736bf75a
sopegue <yayasopeguesoro@gmail.com>
2020-10-31 19:52:31 +0100

    vue

---
+++ src/components/Grand.vue
@@ -0,0 +1,15 @@
+<template>
+  <input type="text" @keyup="$emit('my-keyup', $event, var1, var2)" />
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      var1: "var1",
+      var2: "var2",
+    };
+  },
+};
+</script>
